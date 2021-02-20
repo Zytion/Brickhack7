c80327e6d01594715a2aba981a79f31a0f871683
Skyler Zartman <43190247+treecone@users.noreply.github.com>
2021-02-20 18:19:45 -0500

    Updated hop 2

--- Project C-Sim/Assets/Scripts/Hop.cs
+++ Project C-Sim/Assets/Scripts/Hop.cs
@@ -18,7 +18,11 @@ public class Hop : MonoBehaviour
     {
         if(gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.01f)
         {
-            gameObject.transform.GetChild(0).transform.position = new Vector3(this.transform.position.x, this.transform.position.y + Mathf.Abs(Mathf.Sin(Time.time * speed + seed) * height), this.transform.position.z);
+            gameObject.transform.GetChild(0).transform.position = new Vector3(this.transform.position.x, this.transform.position.y + Mathf.Abs(Mathf.Sin((Time.time * speed + seed)) * height), this.transform.position.z);
+        }
+        else
+        {
+            gameObject.transform.GetChild(0).transform.position = Vector2.Lerp(transform.GetChild(0).transform.position, gameObject.transform.position, 0.1f);
         }
     }
 }
