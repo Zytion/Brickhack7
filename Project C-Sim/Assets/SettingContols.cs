88350a8d292b7f57ded5d790a4076865183cb8e0
Robert <42982719+Zytion@users.noreply.github.com>
2021-02-20 19:21:24 -0500

    Made adjustments to sliders

---
+++ Project C-Sim/Assets/SettingContols.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SettingContols : MonoBehaviour
+{
+	private TMP_InputField inputField;
+	private Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		inputField = gameObject.GetComponentInChildren<TMP_InputField>();
+		slider = gameObject.GetComponentInChildren<Slider>();
+	}
+
+	public void UpdateSlider()
+	{
+		slider.value = int.Parse(inputField.text);
+	}
+
+	public void UpdateInput()
+	{
+		inputField.text = ((int)slider.value).ToString();
+	}
+
+}
