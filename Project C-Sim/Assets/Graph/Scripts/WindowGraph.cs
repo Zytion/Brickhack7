0b0306b8322a8c14d640ab5b7de5e8fbc9dc363b
Robert <42982719+Zytion@users.noreply.github.com>
2021-02-20 22:10:45 -0500

    added data tab to samplescene

--- Project C-Sim/Assets/Graph/Scripts/WindowGraph.cs
+++ Project C-Sim/Assets/Graph/Scripts/WindowGraph.cs
@@ -16,25 +16,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class WindowGraph : MonoBehaviour
 {
 
 	[SerializeField] private Sprite circleSprite;
-	[SerializeField] private Sprite upSprite;
-	[SerializeField] private Sprite downSprite;
+	//[SerializeField] private Sprite upSprite;
+	//[SerializeField] private Sprite downSprite;
 	private RectTransform graphContainer;
 	//public bool showCircles = true;
 	public List<int> infectedValues;
 	public List<int> suseptableValues;
 	private List<GameObject> dots;
 
+	private int highest, hightestDay, population, dead;
+
+	[SerializeField] private List<TextMeshProUGUI> texts;
+
 	private void Awake()
 	{
 		graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
 		dots = new List<GameObject>();
-		infectedValues = new List<int>() { 5, 80, 56, 45, 30, 22, 17, 15, 13, 10, 10, 10, 7, 7, 3 };
-		suseptableValues = new List<int>() { 95, 20, 20, 20, 20, 18, 17, 15, 13, 13, 13, 13, 10, 10, 10 };
+		infectedValues = new List<int>() { 5};
+		suseptableValues = new List<int>() { 95 };
+		population = 100;
+		dead = 0;
+		highest = 0;
+		hightestDay = 0;
 	}
 
 	private void Update()
@@ -102,6 +110,9 @@ public class WindowGraph : MonoBehaviour
 			lastDotGameObject = circleGameObject;
 			dots.Add(circleGameObject);
 		}
+		int sus = sValues[sValues.Count - 1];
+		int infected = iValues[iValues.Count - 1];
+		UpdateData(100 - sus - infected, sus, infected, iValues.Count);
 	}
 
 	private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color color)
@@ -119,41 +130,60 @@ public class WindowGraph : MonoBehaviour
 		rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
 		dots.Add(gameObject);
 	}
-	//WORKS-ISH, but is laggy
-	private void CreateAreaConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color color)
+
+	private void UpdateData(int rm, int sus, int infected, int day)
 	{
-		GameObject barFill = new GameObject("barFill", typeof(Image));
-		GameObject triangleFill = new GameObject("triFill", typeof(Image));
-		barFill.transform.SetParent(graphContainer, false);
-		barFill.GetComponent<Image>().color = color;
-		triangleFill.transform.SetParent(graphContainer, false);
-		triangleFill.GetComponent<Image>().color = color;
-		float yLevel = dotPositionA.y;
-		if (dotPositionA.y > dotPositionB.y)
-		{
-			yLevel = dotPositionB.y;
-			triangleFill.GetComponent<Image>().sprite = downSprite;
-		}
-		else
-		{
-			triangleFill.GetComponent<Image>().sprite = upSprite;
-		}
-		float width = dotPositionB.x - dotPositionA.x;
 		
-		RectTransform rectTransform = barFill.GetComponent<RectTransform>();
-		rectTransform.pivot = new Vector2(0.5f, 1);
-		rectTransform.anchorMin = new Vector2(0, 0);
-		rectTransform.anchorMax = new Vector2(0, 0);
-		rectTransform.sizeDelta = new Vector2(width, yLevel);
-		rectTransform.anchoredPosition = new Vector2(dotPositionA.x + width / 2.0f, yLevel);
+		texts[0].text =  rm + "% removed";
+		texts[1].text =  sus + "% susceptible";
+		texts[2].text =  infected + "% infected";
 
-		rectTransform = triangleFill.GetComponent<RectTransform>();
-		rectTransform.pivot = new Vector2(0.5f, 1);
-		rectTransform.anchorMin = new Vector2(0, 0);
-		rectTransform.anchorMax = new Vector2(0, 0);
-		rectTransform.sizeDelta = new Vector2(width, Math.Abs(dotPositionB.y - dotPositionA.y));
-		rectTransform.anchoredPosition = new Vector2(dotPositionA.x + width / 2.0f, dotPositionA.y);
+		if(infected > highest)
+		{
+			highest = infected;
+			hightestDay = day;
+		}
 
-		dots.Add(barFill);
+		texts[3].text = "Highest infection amount: " +  highest + "%";
+		texts[4].text = "Highest infection day: Day " + hightestDay;
+		texts[5].text = "Estimated Dead: " + infected / 100.0f * population;
 	}
+
+	//WORKS-ISH, but is laggy
+	//private void CreateAreaConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color color)
+	//{
+	//	GameObject barFill = new GameObject("barFill", typeof(Image));
+	//	GameObject triangleFill = new GameObject("triFill", typeof(Image));
+	//	barFill.transform.SetParent(graphContainer, false);
+	//	barFill.GetComponent<Image>().color = color;
+	//	triangleFill.transform.SetParent(graphContainer, false);
+	//	triangleFill.GetComponent<Image>().color = color;
+	//	float yLevel = dotPositionA.y;
+	//	if (dotPositionA.y > dotPositionB.y)
+	//	{
+	//		yLevel = dotPositionB.y;
+	//		triangleFill.GetComponent<Image>().sprite = downSprite;
+	//	}
+	//	else
+	//	{
+	//		triangleFill.GetComponent<Image>().sprite = upSprite;
+	//	}
+	//	float width = dotPositionB.x - dotPositionA.x;
+		
+	//	RectTransform rectTransform = barFill.GetComponent<RectTransform>();
+	//	rectTransform.pivot = new Vector2(0.5f, 1);
+	//	rectTransform.anchorMin = new Vector2(0, 0);
+	//	rectTransform.anchorMax = new Vector2(0, 0);
+	//	rectTransform.sizeDelta = new Vector2(width, yLevel);
+	//	rectTransform.anchoredPosition = new Vector2(dotPositionA.x + width / 2.0f, yLevel);
+
+	//	rectTransform = triangleFill.GetComponent<RectTransform>();
+	//	rectTransform.pivot = new Vector2(0.5f, 1);
+	//	rectTransform.anchorMin = new Vector2(0, 0);
+	//	rectTransform.anchorMax = new Vector2(0, 0);
+	//	rectTransform.sizeDelta = new Vector2(width, Math.Abs(dotPositionB.y - dotPositionA.y));
+	//	rectTransform.anchoredPosition = new Vector2(dotPositionA.x + width / 2.0f, dotPositionA.y);
+
+	//	dots.Add(barFill);
+	//}
 }
